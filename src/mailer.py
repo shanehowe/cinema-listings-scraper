f2a7684de5483f114ce972fe3a2b9bb4fd994f54
Shane Howe <shanehowe100@hotmail.com>
2023-07-07 20:04:04 +0100

    build_html_email() and raise Exception if not env keep mypy happy

--- src/mailer.py
+++ src/mailer.py
@@ -1,16 +1,45 @@
 from smtplib import SMTP
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from dotenv import load_dotenv
+from movie import Movie
 import os
+
+load_dotenv()
 
 
 def get_smtp_server() -> SMTP:
-    load_dotenv()
     server = SMTP("smtp-mail.outlook.com", 587)
     server.starttls()
-    server.login(os.getenv("EMAIL"), os.getenv("PASSWORD"))
+
+    email = os.getenv("EMAIL")
+    password = os.getenv("PASSWORD")
+
+    if not email or not password:
+        raise Exception("No email and/or password found in environment variables.")
+
+    server.login(email, password)
     return server
 
 
-def send_mail_and_quit(server: SMTP) -> None:
-    # TODO: send mail
-    server.quit()
+def build_html_email(movies: list[Movie]) -> MIMEMultipart:
+    message = MIMEMultipart("alternative")
+    message["subject"] = "Cinema Listings 🍿"
+    message["from"] = os.getenv("EMAIL")
+    message["to"] = f"{os.getenv('SHANE')}, {os.getenv('GEORGINA')}"
+
+    movies_html_section = ""
+    for movie in movies:
+        movies_html_section += movie.to_html()
+
+    html_content = f"""
+        <html>
+            <head></head>
+            <body>
+                {movies_html_section}
+            </body>
+        </html>
+    """
+    part = MIMEText(html_content, "html")
+    message.attach(part)
+    return message
