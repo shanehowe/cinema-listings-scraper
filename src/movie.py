5096021ded5e9619dcd1d43af87479be9c74f538
Shane Howe <shanehowe100@hotmail.com>
2023-07-08 12:19:01 +0100

    remove rotten toms score and add imdb score

--- src/movie.py
+++ src/movie.py
@@ -2,6 +2,13 @@ class Movie:
     def __init__(self, title: str, listing_times: list[str]) -> None:
         self._title = title
         self._times = listing_times
+        self._rating = None
+
+    def get_rating(self) -> str | None:
+        return self._rating
+
+    def set_rating(self, rating: str | None) -> None:
+        self._rating = rating
 
     def get_title(self) -> str:
         return self._title
@@ -14,10 +21,15 @@ class Movie:
 
     def to_html(self):
         listing_times = ", ".join(self._times)
+        if self._rating is None:
+            rating = "N/A"
+        else:
+            rating = self._rating
 
         html = f"""
-            <div>
+            <div class="container">
                 <h3>{self._title}</h3>
+                <p>IMDB rating ⭐️: {rating}</p>
                 <p>Listing Times ⏰: {listing_times}</p>
             </div>
         """
