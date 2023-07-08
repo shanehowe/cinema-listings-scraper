9c450fd9af32d6ddcddcd882d70571a488d19713
Shane Howe <shanehowe100@hotmail.com>
2023-07-08 11:20:28 +0100

    remove rotten_toms scraper obey robots.txt

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 import cinema_scraper
-import tomato_scraper
 import mailer
 import os
 import logging
@@ -11,20 +10,11 @@ def main():
     load_dotenv()
     logging.basicConfig(level=logging.INFO)
 
-    cinema_soup = cinema_scraper.get_cinema_soup()
     logging.info("Getting cinema listings")
+    cinema_soup = cinema_scraper.get_cinema_soup()
     cinema_listings = cinema_scraper.get_cinema_listings(cinema_soup)
     movies = cinema_scraper.create_movie_objects(cinema_listings)
-
-    firefox_driver = tomato_scraper.get_headless_browser()
-    for movie in movies:
-        logging.info(f"Getting rotten tomatoes for {movie.get_title()}")
-        rotten_tom_score = tomato_scraper.get_tomato_score(
-            firefox_driver, movie.get_title()
-        )
-        movie.set_rotten_tomato(rotten_tom_score)
-    firefox_driver.close()
-
+    
     logging.info("Trying to send email")
     server = mailer.get_smtp_server()
     email = mailer.build_html_email(movies)
--- src/movie.py
+++ src/movie.py
@@ -2,13 +2,6 @@ class Movie:
     def __init__(self, title: str, listing_times: list[str]) -> None:
         self._title = title
         self._times = listing_times
-        self._rotten_tomatoes: int | None = None
-
-    def get_rotten_tomato(self) -> int | None:
-        return self._rotten_tomatoes
-
-    def set_rotten_tomato(self, rotten_tom_score: int | None) -> None:
-        self._rotten_tomatoes = rotten_tom_score
 
     def get_title(self) -> str:
         return self._title
@@ -20,19 +13,15 @@ class Movie:
         self._times = new_times
 
     def to_html(self):
-        rotten_tomato_score = (
-            f"{self._rotten_tomatoes}%" if self._rotten_tomatoes is not None else "N/A"
-        )
         listing_times = ", ".join(self._times)
 
         html = f"""
             <div>
                 <h3>{self._title}</h3>
-                <p>Rotten tomatoes 🍅: {rotten_tomato_score}</p>
                 <p>Listing Times ⏰: {listing_times}</p>
             </div>
         """
         return html
 
     def __repr__(self) -> str:
-        return f"Movie(\ntitle={self._title}\ntimes={self._times}\n🍅={self._rotten_tomatoes}\n)"
+        return f"Movie(\ntitle={self._title}\ntimes={self._times}\n)"
