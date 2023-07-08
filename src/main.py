ca220f4aa40b72ddf0adedd4c8f7e1d5ed5843c3
Shane Howe <shanehowe100@hotmail.com>
2023-07-08 12:22:38 +0100

    populate move ratings before sending

--- src/main.py
+++ src/main.py
@@ -14,12 +14,19 @@ def main():
     cinema_soup = cinema_scraper.get_cinema_soup()
     cinema_listings = cinema_scraper.get_cinema_listings(cinema_soup)
     movies = cinema_scraper.create_movie_objects(cinema_listings)
-    
+
+    for movie in movies:
+        title = movie.get_title()
+        logging.info(f"Getting rating for {title}")
+        rating = cinema_scraper.get_movie_rating(title)
+        movie.set_rating(rating)
+
     logging.info("Trying to send email")
     server = mailer.get_smtp_server()
     email = mailer.build_html_email(movies)
     from_address = os.getenv("EMAIL")
     to_addresses = [os.getenv("SHANE"), os.getenv("GEORGINA")]
+
     try:
         server.sendmail(from_address, to_addresses, email.as_string())
     except Exception as e:
